3c31a43f5d29092267b45414ba590e3ccee5f7d0
unknown <blanecummings@hotmail.com>
2022-12-31 16:04:09 -0600

    can't remember what all was done because I got sick. DB connection is now working within docker container.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask 
+from db.config import mysql
 
 def create_app():
     app  = Flask(__name__)
+
+    app.config['MYSQL_HOST'] = '172.17.0.1'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_DB'] = 'MainTruckDB'
+    mysql.init_app(app)
 
     from app.login.views import login_blueprint
     app.register_blueprint(login_blueprint)
--- app/login/views.py
+++ app/login/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, request, render_template
+from db.config import mysql
 
 login_blueprint = Blueprint("login", __name__)
+
 @login_blueprint.route("/login")
 def login():
     return render_template("login.html")
 
-@login_blueprint.route("/somethingelse")
-def somethingElse():
-    return "<h1>Hlep</h1>"
+@login_blueprint.route("/login", methods=["POST"])
+def login_post():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Users WHERE Username = %s AND Password = %s", (username, password))
+    user_found = cursor.fetchone()
+
+    if user_found: 
+        render_template("home.html")
+    else:
+        return "<h1>ERROR: User/Password combo not found....</h1>"
